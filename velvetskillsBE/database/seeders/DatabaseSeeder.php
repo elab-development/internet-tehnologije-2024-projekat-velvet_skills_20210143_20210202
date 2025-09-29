5727e0af39fbb5326825d7a364bcf6a12d772849
Anastasija <ad20210143@student.fon.bg.ac.rs>
2025-09-29 11:56:10 +0200

    Zvanje u DB seederu i popunjavanje baze sa db seed

--- velvetskillsBE/database/seeders/DatabaseSeeder.php
+++ velvetskillsBE/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\SkillSeeder;
+use Database\Seeders\UserSkillSeeder;
+use Database\Seeders\CredentialSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+         $this->call([
+        UserSeeder::class,        // Prvo korisnici
+        SkillSeeder::class,       // Zatim veštine (katalog)
+        UserSkillSeeder::class,   // Zatim pivot veza user ↔ skill
+        CredentialSeeder::class,  //  I na kraju credentiali za te veštine
+    ]);
     }
 }
