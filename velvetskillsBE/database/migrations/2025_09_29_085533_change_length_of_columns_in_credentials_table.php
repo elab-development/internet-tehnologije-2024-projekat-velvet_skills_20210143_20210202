f51f383863d406eb70449c898a7f01b1b4e54aec
Anastasija <ad20210143@student.fon.bg.ac.rs>
2025-09-29 11:55:20 +0200

    Dodatne migracije sa nekim sitnim izmenama da bolje budu postavljene tabele

---
+++ velvetskillsBE/database/migrations/2025_09_29_085533_change_length_of_columns_in_credentials_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('credentials', function (Blueprint $table) {
+            $table->string('type', 100)->change();
+            $table->string('title', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('credentials', function (Blueprint $table) {
+            $table->string('type')->change();
+            $table->string('title')->change();
+        });
+    }
+};
