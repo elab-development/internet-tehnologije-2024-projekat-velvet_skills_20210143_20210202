bf19b5ba59b4a068099481932fabb540c12589d3
Anastasija <ad20210143@student.fon.bg.ac.rs>
2025-10-12 15:53:41 +0200

    Promena malo u seederu da bolje radi

--- velvetskillsBE/database/seeders/CredentialSeeder.php
+++ velvetskillsBE/database/seeders/CredentialSeeder.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Credential;
 
-
 class CredentialSeeder extends Seeder
 {
     /**
@@ -15,19 +14,33 @@ class CredentialSeeder extends Seeder
      */
     public function run(): void
     {
-         $users = User::where('role', 'user')->get();
+        // Niz dostupnih slika za kredencijale
+        $fileLinks = [
+            "https://campus.w3schools.com/cdn/shop/files/202_700x700.png?v=1720962295",
+            "https://soimlearningtocode.wordpress.com/wp-content/uploads/2015/08/certificate.jpg",
+            "https://www.typecalendar.com/wp-content/uploads/2023/06/Word-Sample-Diploma-Template.jpg?gid=671",
+            "https://www.typecalendar.com/wp-content/uploads/2023/02/Diploma.jpg",
+            "https://data.templateroller.com/pdf_docs_html/2376/23762/2376294/diploma-certificate-template_big.png",
+            "https://res.cloudinary.com/certifier/image/upload/v1718876081/completion_certificate_templates_Certifier_blog_stylish_758c35f7ac.jpg",
+            "https://res.cloudinary.com/certifier/image/upload/v1748361450/20_3321387fcd.webp",
+        ];
+
+        $users = User::where('role', 'user')->get();
 
         foreach ($users as $user) {
-            // Dohvati veštine koje korisnik već ima kroz pivot tabelu
+            // Sve veštine koje korisnik ima
             $userSkills = $user->userSkills()->with('skill')->get();
 
             foreach ($userSkills as $userSkill) {
-                // 50% šansa da za ovu veštinu postoji credential
+                // 50% šanse da dobije credential
                 if (fake()->boolean(50)) {
                     Credential::factory()->create([
-                        'user_id'  => $user->id,
-                        'skill_id' => $userSkill->skill_id,
-                        'reviewed_by' => User::where('role', 'moderator')->inRandomOrder()->first()?->id,
+                        'user_id'     => $user->id,
+                        'skill_id'    => $userSkill->skill_id,
+                        'file'        => $fileLinks[array_rand($fileLinks)], //Nasumična slika
+                        'reviewed_by' => User::where('role', 'moderator')
+                            ->inRandomOrder()
+                            ->first()?->id,
                     ]);
                 }
             }
